ad49062b7b2304a933bdd6eade540de074d72ed2
markkket <92202536+markkket@users.noreply.github.com>
2021-11-27 01:10:19 +0300

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+/// <summary>
+/// вычисляет сумму ряда
+/// </summary>
+/// <param name="k">стоппер</param>
+void calculateSumOfSeries(int k) 
+{
+	float s = 0;
+	for (float n = 1; n <= k; n++) 
+	{
+		s += 2 / ((2 * n + 1) * (2 * n + 3));
+	}
+	cout << "summa = "<<s<< endl;
+}
+
+int main()
+{
+	int k;
+	cout << "enter k" << endl;
+	cin >> k;
+	calculateSumOfSeries(k);
+	return 0;
+}
