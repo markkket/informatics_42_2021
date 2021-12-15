d841319b991cd596beb3ea9a7ade49f631ff100c
markkket <92202536+markkket@users.noreply.github.com>
2021-12-15 21:23:27 +0300

    Add files via upload

---
+++ nok.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int nod(int, int);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, t;
+	int nok = 0;
+	cout << "введите числа\n";
+	cin >> a >> b;
+	t = nod(a, b);
+	cout << "НОД=" << t << "\n";
+
+	nok = a * b / t;
+	cout << "НОК=" << nok << "\n";
+	return 0;
+}
+int nod(int a, int b)
+{
+	while (a != b)
+	{
+		if (a > b)
+			a = a - b;
+		else b = b - a;
+	}
+	return a;
+}
